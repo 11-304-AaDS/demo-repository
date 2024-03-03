a4f69e7940b38afb013730a7d8f6648247ba6035
yaildarr <ildartanki@mail.ru>
2024-03-03 21:55:50 +0300

    gotovo

--- Queue/Main.java
+++ Queue/Main.java
@@ -2,6 +2,28 @@ package Queue;
 
 public class Main {
     public static void main(String[] args) {
-
+        myQueue<Integer> queue = new myQueue<>(5);
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        queue.push(5);
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        queue.push(1);
+        System.out.println(queue);
+        System.out.println(queue.peek());
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        System.out.println(queue.isEmpty());
     }
 }
--- Queue/myQueue.java
+++ Queue/myQueue.java
@@ -1,4 +1,62 @@
 package Queue;
 
-public class myQueue {
+import java.util.NoSuchElementException;
+
+public class myQueue<T> {
+    private int top = -1;
+    private int end = -1;
+    private int capacity;
+    private Object[] array;
+
+    public myQueue(int n) {
+        array = new Object[n];
+    }
+
+    public void push(T e) {
+        if (top == -1) {
+            array[0] = e;
+            top++;
+            end++;
+        } else {
+            end = (end + 1) % array.length;
+            array[end] = e;
+        }
+        capacity++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            T temp = (T) array[top];
+            array[top] = null;
+            capacity--;
+            if (capacity == 0) {
+                top = -1;
+                end = -1;
+                return temp;
+            }
+            top = (top + 1) % array.length;
+            return temp;
+        }
+    }
+
+    public boolean isEmpty() {
+        return capacity == 0;
+    }
+    public T peek(){
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        } else{
+            return (T) array[top];
+        }
+    }
+    public String toString(){
+        String temp = "";
+        for (int i = 0; i < array.length; i++) {
+            temp += array[i];
+            temp += " ";
+        }
+        return temp;
+    }
 }
