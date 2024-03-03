d465e36d6c6b2672bbaaa13b72ffb76a2aed4950
makarjaan <154356563+makarjaan@users.noreply.github.com>
2024-03-03 19:02:55 +0300

    Delete EmtyException.java

--- EmtyException.java
+++
@@ -1,7 +0,0 @@
-package les3.QueueDz;
-
-public class EmtyException extends Exception{
-    public EmtyException(){
-        super("из очереди нельзя ничего удалить, так как в нём нет элементов");
-    }
-}
