9743d33d4057ea0675ee03348001c7f370b0983c
makarjaan <154356563+makarjaan@users.noreply.github.com>
2024-03-03 19:03:11 +0300

    Delete FullException.java

--- FullException.java
+++
@@ -1,7 +0,0 @@
-package les3.QueueDz;
-
-public class FullException extends Exception{
-    public FullException(){
-        super("в очередь нельзя добавить новый элемент, так как он уже переполнен");
-    }
-}
