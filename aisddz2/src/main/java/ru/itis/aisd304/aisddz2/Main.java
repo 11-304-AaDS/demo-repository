de87228054818fde6ef694e6d5baad0c8d8edd2a
ELxander7 <panda925milano@gmail.com>
2024-03-10 19:21:24 +0300

    add second dz

---
+++ aisddz2/src/main/java/ru/itis/aisd304/aisddz2/Main.java
@@ -0,0 +1,39 @@
+package ru.itis.aisd304.aisddz2;
+
+public class Main {
+    public static void main(String[] args) {
+        QueueByTwoStacks<Integer> queue = new QueueByTwoStacks<>();
+
+        queue.push(7);
+        queue.push(5);
+        queue.push(1);
+        queue.push(2);
+
+        System.out.println(queue.toString());
+
+        System.out.println(queue.empty());
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue.pop());
+        System.out.println(queue.toString());
+        System.out.println(queue.pop());
+        System.out.println(queue.toString());
+        System.out.println(queue.pop());
+        System.out.println(queue.toString());
+        System.out.println(queue.pop());
+
+        System.out.println(queue.empty());
+        System.out.println(queue.toString());
+
+        queue.push(8);
+        System.out.println(queue.toString());
+        queue.push(9);
+        System.out.println(queue.toString());
+        queue.push(10);
+
+        System.out.println(queue.toString());
+        System.out.println(queue.empty());
+
+    }
+}
