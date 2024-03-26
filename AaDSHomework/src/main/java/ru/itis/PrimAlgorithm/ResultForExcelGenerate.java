e7776eb0e0144f7e4270638eb7e912810bb3ac4d
Sayid <sayid.salizhanov@mail.ru>
2024-03-26 23:02:00 +0300

    Update ResultForExcelGenerate.java

--- AaDSHomework/src/main/java/ru/itis/PrimAlgorithm/ResultForExcelGenerate.java
+++ AaDSHomework/src/main/java/ru/itis/PrimAlgorithm/ResultForExcelGenerate.java
@@ -1,13 +1,10 @@
 package ru.itis.PrimAlgorithm;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class resultForExcelGenerate {
+public class ResultForExcelGenerate {
     public static void main(String[] args) throws IOException {
         File testFile = new File("resultFile.txt");
         testFile.createNewFile();
