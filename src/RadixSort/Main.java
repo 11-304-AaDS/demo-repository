cee398bbd47f7771d66b9f2afdbc3bc05f4bd99a
kulsi <kulsitova05@inbox.ru>
2024-03-27 23:49:44 +0300

    скорректированный

--- src/RadixSort/Main.java
+++ src/RadixSort/Main.java
@@ -5,18 +5,25 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int count = 6;
+
+        int count = 1;
+
+        long[] time = new long[count];
 
         for(int i = 1; i < count + 1; i++) {
+            long startTime = System.nanoTime();
             Generator generator = new Generator();
             File file = generator.createFile(i);
             Algorithm radixSort = new Algorithm(file);
             radixSort.sorting();
             file.delete();
-            for (Integer data : radixSort.dataSet) {
-                System.out.println(data);
-            }
             radixSort.setInFile(i);
+            long finishTime = System.nanoTime();
+            time[i-1] = (finishTime - startTime)/(long)1000000;
+        }
+
+        for (long t : time) {
+            System.out.println(t);
         }
     }
 }
