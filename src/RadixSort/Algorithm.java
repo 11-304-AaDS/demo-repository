0e23bc2bd9b8811c67ddc50a61b9d9b8c3171f80
kulsi <kulsitova05@inbox.ru>
2024-03-30 16:47:31 +0300

    окончательная версия + отсортированные файлы

--- src/RadixSort/Algorithm.java
+++ src/RadixSort/Algorithm.java
@@ -6,28 +6,35 @@ import java.util.*;
 public class Algorithm {
     Deque<Integer> dataSet;
     LinkedList<Integer>[] radixes = new LinkedList[10];
+    int countOfIterations;
 
     Algorithm(File file) throws FileNotFoundException {
         this.dataSet = reading(file);
+        this.countOfIterations = 0;
         for (int i = 0; i < 10; i++) {
             radixes[i] = new LinkedList<>();
         }
     }
 
-    public void sorting() {
+    public long sorting() {
+        long startTime = System.nanoTime();
         int max = Collections.max(dataSet);
         int a = 1;
         while (a <= max) {
             while (!dataSet.isEmpty()) {
                 Integer data = dataSet.pop();
                 radixes[(data % (a * 10)) / a].add(data);
+                countOfIterations++;
             }
             for (int i = 0; i < 10; i++) {
                 dataSet.addAll(radixes[i]);
                 radixes[i].clear();
             }
             a *= 10;
+            countOfIterations ++;
         }
+        long finishTime = System.nanoTime();
+        return finishTime - startTime;
     }
 
     public void setInFile(int i) throws IOException {
--- src/RadixSort/Main.java
+++ src/RadixSort/Main.java
@@ -6,24 +6,29 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        int count = 1;
+        int count = 100;
 
         long[] time = new long[count];
+        int [] amountOfElements = new int[count];
+        int[] amountOfIterations = new int[count];
 
         for(int i = 1; i < count + 1; i++) {
-            long startTime = System.nanoTime();
             Generator generator = new Generator();
             File file = generator.createFile(i);
             Algorithm radixSort = new Algorithm(file);
-            radixSort.sorting();
+            amountOfElements[i-1] = radixSort.dataSet.size();
+            long timeOfAlgorithm = radixSort.sorting()/(long) 1000000;
+            amountOfIterations[i-1] = radixSort.countOfIterations;
             file.delete();
             radixSort.setInFile(i);
-            long finishTime = System.nanoTime();
-            time[i-1] = (finishTime - startTime)/(long)1000000;
+            time[i-1] = timeOfAlgorithm;
         }
 
-        for (long t : time) {
-            System.out.println(t);
+        for (int i = 0; i < count; i++) {
+            System.out.print(time[i] + ";");
+            System.out.print(amountOfElements[i] + ";");
+            System.out.print(amountOfIterations[i]);
+            System.out.println();
         }
     }
 }
