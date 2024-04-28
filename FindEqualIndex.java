1ab5b21268f82aa7a73813f1bf9d1b30929e843a
damir <damir240205@mail.ru>
2024-04-29 00:58:42 +0300

    homework done

---
+++ FindEqualIndex.java
@@ -0,0 +1,24 @@
+
+public class FindEqualIndex {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[]{-2, 0, 2, 4, 6, 8, 10};;
+        System.out.println(FindEqualIndex(array));
+    }
+
+    public static Integer FindEqualIndex(Integer[] test) {
+        int left = 0;
+        int right = test.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (test[mid] == mid) {
+                return mid;
+            }
+            if (test[mid] < mid) {
+                left = mid + 1;
+            } else if (test[mid] > mid) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
