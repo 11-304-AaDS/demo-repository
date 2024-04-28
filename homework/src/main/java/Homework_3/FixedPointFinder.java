4f5e0521e71be5caf15d0eb3d332c0c5dd4aa64a
Dinislam-zak <danvarro18@gmail.com>
2024-04-28 20:51:42 +0300

    Commit:D

---
+++ homework/src/main/java/Homework_3/FixedPointFinder.java
@@ -0,0 +1,32 @@
+package Homework_3;
+
+public class FixedPointFinder {
+    public static int findFixedPoint(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == mid) {
+                return mid;
+            } else if (arr[mid] < mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-10, -5, 0, 3, 7, 9, 11, 12, 13};
+        int result = findFixedPoint(arr);
+        if (result != -1) {
+            System.out.println("Индекс: " + result);
+        } else {
+            System.out.println("Такого i не существует");
+        }
+    }
+}
