ef51972b0ce713fcb082926deb6e8ed78aab3864
Alex Kirilin <kirilin.75.ru@gmail.com>
2024-04-28 23:33:59 +0300

    added fixes to the homework(misunderstandings of the task)

--- Homework 28.04/BinarySearch.java
+++ Homework 28.04/BinarySearch.java
@@ -3,19 +3,19 @@ import java.util.List;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>(List.of( -120, -109, -97, -84, -67, -56, -45, -39, -33, -25, -10, -8, 5, 17, 39, 64, 76, 92, 98, 123));
-        System.out.println(binarySearch(list, 5));
+        LinkedList<Integer> list = new LinkedList<>(List.of(-49, -1, 2, 34, 51, 228, 1488, 5000, 6000, 7000));
+        System.out.println(binarySearch(list));
     }
 
-    public static int binarySearch(List<Integer> list, Integer element) {
+    public static int binarySearch(List<Integer> list) {
         int left = 0;
         int right = list.size() - 1;
 
         while(left <= right) {
-            int middle = (left + right) / 2;
-            if (element.compareTo(list.get(middle)) > 0) {
+            Integer middle = (left + right) / 2;
+            if (middle.compareTo(list.get(middle)) > 0) {
                 left = middle + 1;
-            } else if (element.compareTo(list.get(middle)) < 0) {
+            } else if (middle.compareTo(list.get(middle)) < 0) {
                 right = middle - 1;
             } else {
                 return middle;
