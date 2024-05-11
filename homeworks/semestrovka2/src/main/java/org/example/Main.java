e9013b2df2b2616a816f8d20726690e64676c843
Nbveh25 <tima.bikmukhametov@inbox.ru>
2024-05-11 16:52:36 +0300

    Update Main.java

--- homeworks/semestrovka2/src/main/java/org/example/Main.java
+++ homeworks/semestrovka2/src/main/java/org/example/Main.java
@@ -16,8 +16,8 @@ public class Main {
 
         // 3. Поэлементно добавьте числа в структуру, замеряя время работы и
         // количество операций для каждого добавления.
-        //int[] array1 = TestGenerator.getArray();
-        int[] array1 = new int[]{2,4,1,9,8,3,5};
+        int[] array1 = TestGenerator.getArray();
+        //int[] array1 = new int[]{2,4,1,9,8,3,5};
         for (int i = 0; i < array1.length; i++) {
             RBTree.add(array1[i]);
             mid_count += RBTree.getCounter();
@@ -32,7 +32,7 @@ public class Main {
 
         //4. Случайным образом выберите из массива 100 элементов и найдите их в структуре,
         // замеряя также время работы и количество операций для каждого поиска.
-        /*int[] array2 = TestGenerator.getArrayByCount(100);
+        int[] array2 = TestGenerator.getArrayByCount(100);
         for (int i = 0; i < array2.length; i++) {
             RBTree.search(array2[i]);
             mid_count += RBTree.getCounter();
@@ -44,12 +44,12 @@ public class Main {
 
         mid_count = 0;
         mid_time = 0;
-*/
+
         //5. Случайным образом выберите из массива 1000 элементов и удалите их из структуры,
         // замеряя время работы и количество операций для каждого удаления.
         int[] array3 = TestGenerator.getArrayByCount(1000);
-        for (int i = 0; i < array1.length; i++) {
-            RBTree.remove(array1[i]);
+        for (int i = 0; i < array3.length; i++) {
+            RBTree.remove(array3[i]);
             mid_count += RBTree.getCounter();
             mid_time += RBTree.getTime();
         }
